3a92dbfd2c34f2ed960792397a9ef22ad3b583db
anthumchris <git@batman.dev>
2025-12-19 08:33:41 +0000

    gothanks: add 0.5.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -5,6 +5,12 @@ class Gothanks < Formula
   sha256 "932915b470a3c55edf6b6524d0062a762ae79c2c9b39685aa1a53aae53449265"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/anthumchris/homebrew-tt/releases/download/gothanks-0.5.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "026947bb34bf577aeef32cac9e3288bf0554a1c022be1bcc1228a53f3209bb3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d4a29cfbfbf3ec448d09be6454874af7bdaf248862a77f3a1a7c922acc6b03d6"
+  end
+
   depends_on "go" => :build
 
   def install
